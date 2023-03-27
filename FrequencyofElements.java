b7bfc7cb2d337c40d9825de3fe166b55012674a6
sukanya naik <95672385+sukanya2712@users.noreply.github.com>
2023-03-27 07:22:03 +0530

    Update FrequencyofElements.java

--- FrequencyofElements.java
+++ FrequencyofElements.java
@@ -9,13 +9,33 @@ public class FrequencyofElements {
         System.out.print("Enter the length of the array: ");
         int length = sc.nextInt();
 
-        int[] arr = new int[length];
+        int[] array = new int[length];
 
         System.out.println("Enter the elements of the array:");
         for (int i = 0; i < length; i++) {
-            arr[i] = sc.nextInt();
+            array[i] = sc.nextInt();
         }
 
+        int[] visitedArray = new int[limit];
 
+        int visited = -1;
+        for (int i = 0; i < limit; i++) {
+            if (visitedArray[i] != visited) {
+                int count = 1;
+                for (int j = i + 1; j < limit; j++) {
+                    if (array[i] == array[j]) {
+                        count++; // 2
+                        visitedArray[j] = visited;
+                    }
+                }
+                visitedArray[i] = count;
+            }
+        }
+        System.out.println("**********************************");
+        System.out.println("  Elements : Frequency");
+        for (int i = 0; i < visitedArray.length; i++) {
+            if (visitedArray[i] != visited)
+                System.out.println("     " + array[i] + "     :     " + visitedArray[i]);
+        }
     }
 }
